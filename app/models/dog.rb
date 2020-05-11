41792a0aeb0ed83bdbadfc6a441ed23f39095dee
German Cruz <cruzgerman216@yahoo.com>
2020-05-11 20:19:42 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,10 +1,16 @@
 class Dog
   attr_accessor :name, :dog, :age
 
+  @@all = []
   def initialize(name, dog, age)
     @name = name
     @dog = dog
     @age = age
+    @@all << self
+  end
+
+  def self.all 
+    @@all
   end
 
 end
